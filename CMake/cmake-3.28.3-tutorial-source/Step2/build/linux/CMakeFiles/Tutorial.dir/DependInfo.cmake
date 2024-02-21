7b6bb8fad22b6523bc2a6ffb8bace992f1eeb9fd
leo <leo20230203@gmail.com>
2024-02-21 15:14:17 +0800

    add cmake-tutorial-step2

---
+++ CMake/cmake-3.28.3-tutorial-source/Step2/build/linux/CMakeFiles/Tutorial.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/leo/workspace/Study/study-notes/CMake/cmake-3.28.3-tutorial-source/Step2/tutorial.cxx" "CMakeFiles/Tutorial.dir/tutorial.cxx.o" "gcc" "CMakeFiles/Tutorial.dir/tutorial.cxx.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/leo/workspace/Study/study-notes/CMake/cmake-3.28.3-tutorial-source/Step2/build/linux/MathFunctions/CMakeFiles/MathFunctions.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
